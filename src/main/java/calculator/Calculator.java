c4c635b5251842f40cd91a377a28de6c6cf34065
KoustuvP <kmukherjee564@gmail.com>
2021-01-10 17:13:52 +0530

    adding supports for two operands

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,7 +5,14 @@ public class Calculator {
 	public int add(String numbers) {
 		if (numbers.isBlank())
 			return 0;
-		return Integer.parseInt(numbers);
+		else {
+			int separatorIndex = numbers.indexOf(",");
+			if (separatorIndex > 0)
+				return Integer.parseInt(numbers.substring(0, separatorIndex))
+						+ Integer.parseInt(numbers.substring(separatorIndex + 1, numbers.length()));
+			else
+				return Integer.parseInt(numbers);
+		}
 	}
 
 }
